adb1ce42649b0c9ea7ca48305756ba3ddf655931
root <ibrahimabdulquddus51@gmail.com>
2022-09-09 17:15:22 -0700

    prints the lowercase alphabet in reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main -  prints the lowercase alphabet in reverse
+ *
+ * Return: Akways 0 (success)
+ */
+int main(void)
+{
+	int q;
+for (q = 'z'; q >= 'a'; a--)
+{
+	putchar(q);
+}
+putchar('\n');
+return (0);
+}
