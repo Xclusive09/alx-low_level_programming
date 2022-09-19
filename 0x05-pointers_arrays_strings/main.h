6aaff99d72a84037a4d16962c73c64692c778999
root <ibrahimabdulquddus51@gmail.com>
2022-09-19 15:14:36 -0700

    print string in reverse

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,11 +1,4 @@
-#ifndef MAIN_H
-#define MAIN_H
-
-/**
-* void prototype()
-* int prototype()
-*/
-
+int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
@@ -14,8 +7,6 @@ void print_rev(char *s);
 void rev_string(char *s);
 void puts2(char *str);
 void puts_half(char *str);
-void print_array(int *a, int *n);
+void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
-int _atoi(char *str);
-
-#endif /*MAIN_H*/
+int _atoi(char *s);/
