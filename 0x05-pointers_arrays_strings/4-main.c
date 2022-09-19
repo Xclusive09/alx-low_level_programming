b6646e22fc05e3872ae77bfcec9217b5044be5c4
root <ibrahimabdulquddus51@gmail.com>
2022-09-19 15:09:09 -0700

    print string in reverse

---
+++ 0x05-pointers_arrays_strings/4-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_rev - print a string in reverse
+ * @s: pointeer to the string
+ * Return: Always 0
+ */
+void print_rev(char *s)
+{
+char *str;
+str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
+
+print_rev(str);
+return (0);
+}
+
