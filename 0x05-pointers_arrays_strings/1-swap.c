d3af17ab6470b89d10c9f7f956d4a50d7d51d0cd
root <ibrahimabdulquddus51@gmail.com>
2022-09-19 13:08:34 -0700

    swap the value of 2 function

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * swap_int -  swap the value of a and b
+ * @a: pointer 1
+ * @b: pointer 2
+ */
+void swap_int(int *a, int *b)
+{
+	*a += *b;
+	*b = *a - *b;
+	*a = *a - *b;
+}
